bc1733e181712ca9d9bc3508daa43d5d5c6c263f
Vladko1973 <vpk1973@yandex.ru>
2023-07-14 22:20:11 +0300

    testng secong commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,35 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldCalculateForAmount900AndGetExtra100() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateForAmount1000AndGetExtraNull() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1_000;
+        int actual = service.remain(1_000);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateForAmount9100AndGetExtra100() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(9_100);
+
+        assertEquals(actual, expected);
+    }
 }
