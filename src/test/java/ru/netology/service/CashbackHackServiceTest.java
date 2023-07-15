ca03b9a3e377bc7b55bf065af9be177fa88ad36a
Vladko1973 <vpk1973@yandex.ru>
2023-07-15 21:47:34 +0300

    first commit junit4_platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,18 +19,56 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCalculateForAmount1000AndGetExtraNull() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 1_000;
+        int expected = 0;
         int actual = service.remain(1_000);
 
         assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldCalculateForAmount9100AndGetExtra100() {
+    public void shouldCalculateForAmount9200AndGetExtra800() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 900;
-        int actual = service.remain(9_100);
+        int expected = 800;
+        int actual = service.remain(9_200);
 
         assertEquals(actual, expected);
     }
+
+    //junit-jupiter tests
+    @Test
+    public void shouldCalculateForAmount1100AndGetExtra900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(1100);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForAmount100AndGetExtra900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(100);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForAmount100000AndGetExtraNull() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(100000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForAmountNullAndGetExtra1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
